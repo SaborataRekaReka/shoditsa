6f9ec5c974f239bf80dd14c61e08fb4eb56f1a91
Your Name <breneize@gmail.com>
2026-07-20 23:38:34 +0500

    Fix Danetki setting migration timestamp

--- packages/database/migrations/0019_enable_danetki_mode.sql
+++ packages/database/migrations/0019_enable_danetki_mode.sql
@@ -2,7 +2,7 @@ UPDATE "app_settings"
 SET
   "value" = 'true'::jsonb,
   "version" = "version" + 1,
-  "updated_at" = now()
+  "updatedAt" = now()
 WHERE "key" = 'danetki.enabled'
   AND "value" = 'false'::jsonb
   AND "updated_by" IS NULL;
